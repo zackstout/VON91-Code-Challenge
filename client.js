4e71cc5ca2ba1a7b5ce29021faa22709eaaff41d
Zack Stout <zackstout@gmail.com>
2018-02-13 10:28:14 -0600

    almost done fiddling with SVG

--- client.js
+++ client.js
@@ -3,6 +3,7 @@
 var pics = [ 'https://i.ytimg.com/vi/o4PDgTV2WhA/hqdefault.jpg', 'https://i.imgur.com/MaoFf8i.jpg', 'https://www.dogbreedinfo.com/images8/PUGtubbs.jpg'];
 var picIndex = 0;
 
+
 // var pic0 = pics[0];
 // var pic1 = pics[1];
 // var pic2 = pics[2];
@@ -35,20 +36,20 @@ function goBack() {
   if (picIndex === -1) {
     picIndex = len - 1;
   }
-  // $('#pics').css('background-image', 'url(' + pics[picIndex] + ')');
-  console.log('back', picIndex);
-  var pic = '#pic' + picIndex + 'Container';
-  console.log($(pic).text());
-  $(pic).show();
-  for (var i=0; i < len; i++) {
-    if (i != picIndex) {
-      var picnow = '#pic' + i + 'Container';
-      $(picnow).hide();
-    }
-  }
-  $(pic).animate({
-    left: "+= 500"
-  }, 1000);
+  $('#pics').css('background-image', 'url(' + pics[picIndex] + ')');
+  // console.log('back', picIndex);
+  // var pic = '#pic' + picIndex + 'Container';
+  // console.log($(pic).text());
+  // $(pic).show();
+  // for (var i=0; i < len; i++) {
+  //   if (i != picIndex) {
+  //     var picnow = '#pic' + i + 'Container';
+  //     $(picnow).hide();
+  //   }
+  // }
+  // $(pic).animate({
+  //   left: "+= 500"
+  // }, 1000);
 }
 
 function goForward() {
